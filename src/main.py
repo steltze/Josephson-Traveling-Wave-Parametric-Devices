23aff62c7bb190463505073f496d8fe843132874
steltze <stel.tze09@gmail.com>
2026-05-19 09:15:55 +0200

    Start symbolic file

--- src/main.py
+++ src/main.py
@@ -4,5 +4,19 @@ def main():
     Zs = Immittance(L=35.5, C=20.1, in_series=False)
     print(Zs.C)
 
+
+
+#     # 3 JJs in series
+# Series(JJ(L), JJ(L), JJ(L))          # == JJ(3L)
+
+# # 3 JJs in parallel  
+# Parallel(Series(JJ(L), JJ(L), JJ(L)), Capacitor(C_g))        # == JJ(L/3)
+
+# # Two-mode Δ shunt
+# Parallel(Capacitor(C_g), Capacitor(2*C_i))
+
+# # SNAIL (future — just add a SNAIL class with phi^3 potential)
+# Series(SNAIL(L_small, L_large, n=3, flux=0.5))
+
 if __name__ == '__main__':
     main()
